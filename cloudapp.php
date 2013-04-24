aad89c90086bddac92431349ae6db99a637e657f
Vitaliy Pitvalo <vitaliy@ekreative.com>
2013-04-25 00:55:35 +0300

    update api

--- cloudapp.php
+++ cloudapp.php
@@ -3,16 +3,19 @@
 class Cloudapp {
 
     private $ch;
+    private $username;
+    private $password;
 
-    public function __construct($name, $password) {
+    public function __construct($username, $password) {
         $this->ch = curl_init();
-
+        $this->username = $username;
+        $this->password = $password;
         curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($this->ch, CURLOPT_HTTPHEADER, array('Accept: application/json'));
-         
+
         curl_setopt($this->ch, CURLOPT_HTTPAUTH, CURLAUTH_DIGEST);
-        curl_setopt($this->ch, CURLOPT_USERPWD, $name . ':' . $password);
+        curl_setopt($this->ch, CURLOPT_USERPWD, $username . ':' . $password);
     }
 
     public function getListItems($params = array('page' => 1, 'perPage' => 10, 'type' => null, 'deleted' => null, 'source' => null)) {
@@ -33,11 +36,11 @@ class Cloudapp {
         $responce = $this->uploadRequest();
         if ($responce->uploads_remaining > 0) {
             $curl = curl_init();
+            curl_setopt($curl, CURLOPT_URL, $responce->url);
             curl_setopt($curl, CURLOPT_HEADER, 1);
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
             curl_setopt($curl, CURLOPT_POST, 1);
-            curl_setopt($curl, CURLOPT_URL, $responce->url);
 
             $params = array(
                 'key' => $responce->params->key,
@@ -51,12 +54,11 @@ class Cloudapp {
 
             curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
             $output = curl_exec($curl);
+            if ($output === false) {
+                echo 'Ошибка curl: ' . curl_error($curl);
+            }
             $location = $this->getLocation($output);
-            echo $location;
-           $output = $this->confirmFile($location);
-           
-           
-           echo $output;
+            $output = $this->confirmFile($location);
             return json_decode($output);
         }
         return false;
@@ -73,22 +75,109 @@ class Cloudapp {
     }
 
     private function confirmFile($location) {
-        
-        curl_setopt($this->ch, CURLOPT_URL, $location);
-        curl_setopt($this->ch, CURLOPT_HEADER, 1);
-        $output = curl_exec($this->ch);
-        curl_setopt($this->ch, CURLOPT_HEADER, 0);
-        echo $output;
-        return json_decode($output);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, trim($location));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json'));
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_DIGEST);
+        curl_setopt($ch, CURLOPT_USERPWD, $this->username . ':' . $this->password);
+        $output = curl_exec($ch);
+        return $output;
     }
 
     private function getLocation($headers) {
         $headers = explode("\n", $headers);
-        return str_replace('Location: ', '', $headers[7]);
+        $url = str_replace('Location: ', '', $headers['7']);
+        return trim($url);
     }
 
     private function uploadRequest() {
-        curl_setopt($this->ch, CURLOPT_URL, 'http://my.cl.ly/items/new?item[private]=false');
+        curl_setopt($this->ch, CURLOPT_URL, 'http://my.cl.ly/items/new');
+        $output = curl_exec($this->ch);
+        return json_decode($output);
+    }
+
+    public function getChangeDefaultSecurity() {
+        return $this->changeDefaultSecurity;
+    }
+
+    public function getChangeEmail() {
+        return $this->changeEmail;
+    }
+
+    public function getChangePassword() {
+        return $this->changePassword;
+    }
+
+    public function getForgotPassword() {
+        return $this->forgotPassword;
+    }
+
+    public function getRegister() {
+        return $this->register;
+    }
+
+    public function getSetCustomDomain() {
+        return $this->setCustomDomain;
+    }
+
+    public function getViewAccountDetails() {
+        curl_setopt($this->ch, CURLOPT_URL, 'http://my.cl.ly/account');
+        $output = curl_exec($this->ch);
+        return json_decode($output);
+    }
+
+    public function getViewAccountStats() {
+        return $this->viewAccountStats;
+    }
+
+    public function getBookmarkLink() {
+        return $this->bookmarkLink;
+    }
+
+    public function getBookmarkMultipleLinks() {
+        return $this->bookmarkMultipleLinks;
+    }
+
+    public function getChangeSecurityofItem() {
+        return $this->changeSecurityofItem;
+    }
+
+    public function getDeleteItem() {
+        return $this->deleteItem;
+    }
+
+    public function getListItemsbySource() {
+        return $this->listItemsbySource;
+    }
+
+    public function getRecoverDeletedItem() {
+        return $this->recoverDeletedItem;
+    }
+
+    public function getRenameItem() {
+        return $this->renameItem;
+    }
+
+    public function getStreamItems() {
+        return $this->streamItems;
+    }
+
+    public function getUploadFile() {
+        return $this->uploadFile;
+    }
+
+    public function getUploadFileWithSpecificPrivacy() {
+        return $this->uploadFileWithSpecificPrivacy;
+    }
+
+    public function getViewDomainDetails() {
+        return $this->viewDomainDetails;
+    }
+
+    public function getViewItem($itemUrl) {
+        curl_setopt($this->ch, CURLOPT_URL, $itemUrl);
         $output = curl_exec($this->ch);
         return json_decode($output);
     }
--- test.php
+++ test.php
@@ -2,18 +2,20 @@
 
 require_once './cloudapp.php';
 require_once './config.php';
-
-
-
-
-$app = new Cloudapp(USERNAME , PASSWORD);
+$app = new Cloudapp(USERNAME, PASSWORD);
 
 echo'<pre>';
 
+//print_r($app->uploadFile(pathinfo(__FILE__, PATHINFO_DIRNAME) . DIRECTORY_SEPARATOR . 'testuploadfile1.jpg'));
+print_r($app->getAccountStats());
+print_r($app->getViewAccountDetails());
 
-//print_r($app->getAccountStats());
-//print_r($app->getListItems());
 
-print_r($app->uploadFile('testuploadfile1.jpg'));
+$items = $app->getListItems();
+foreach($items as $item){
+//    $item = $app->getViewItem($item->url);
+//    print_r($item);
+};
 
+//getViewItem
 echo'</pre>';
